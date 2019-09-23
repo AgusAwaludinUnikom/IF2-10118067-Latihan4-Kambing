cc329fdfc9aafe0b4b8b58c8583ab735d96eb3a0
Agus Awaludin <55693286+AgusAwaludinUnikom@users.noreply.github.com>
2019-09-23 18:26:09 +0700

    Add files via upload

---
+++ src/if2/pkg10118067/latihan4/IF210118067Latihan4.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10118067.latihan4;
+
+/**
+ *
+ * @author
+ * Nama     : Agus Awaludin
+ * Kelas    : IF-2
+ * NIM      : 10118067
+ * Latihan 4. Kambing
+ */
+public class IF210118067Latihan4 {
+    
+    public void tambahKambing() {
+        //deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing setelah ditambah: " + jumlahKambing);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        IF210118067Latihan4 kambingJantan = new IF210118067Latihan4();
+        kambingJantan.tambahKambing();
+    }
+    
+}
